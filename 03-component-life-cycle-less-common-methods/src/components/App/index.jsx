15c9c7068c4d09db3f9ca64ec7cbbe8280b529d9
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-13 14:55:09 +0200

    Training on getDerivedStateFromProps method

--- 03-component-life-cycle-less-common-methods/src/components/App/index.jsx
+++ 03-component-life-cycle-less-common-methods/src/components/App/index.jsx
@@ -3,11 +3,21 @@ import styles from './style.module.css';
 import MyComponent from '../MyComponent';
 
 class App extends Component {
+  state = {
+    age: 27
+  };
+
+  addOneYear = () => {
+    this.setState((prevState) => ({
+      age: prevState.age + 1
+    }));
+  };
+
   render() {
     return (
-      <div className={styles.app}>
-        <p>Hello from App Component</p>
-        <MyComponent />
+      <div className={styles.App}>
+        <MyComponent age={this.state.age} />
+        <button onClick={this.addOneYear}>Change props</button>
       </div>
     );
   }
--- 03-component-life-cycle-less-common-methods/src/components/MyComponent/index.jsx
+++ 03-component-life-cycle-less-common-methods/src/components/MyComponent/index.jsx
@@ -1,12 +1,48 @@
-// Import styles
 import { Component } from 'react';
 import styles from './style.module.css';
 
 class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'SpiderMan'
+    };
+
+    console.log('Constructor');
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps');
+
+    console.log(props);
+    console.log(state);
+    return null;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+
+  toggleForceUpdate = () => {
+    console.log('Force Update');
+    this.forceUpdate(() => {});
+  };
+
   render() {
+    console.log('Render');
     return (
-      <div className={styles.app}>
-        <p>Hello from MyComponent</p>
+      <div className={styles.MyComponent}>
+        <p>
+          <strong>Nom: </strong>
+          {this.state.name}
+        </p>
+        <p>
+          <strong>Age: </strong>
+          {this.props.age}
+        </p>
+        <button onClick={this.toggleForceUpdate}>Force Update</button>
       </div>
     );
   }
