633cc11a410785a53b1be02f0ed53a93256e2b69
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-20 23:21:55 +0200

    Initialize useState training project

---
+++ 10-useState/src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import MyComponent from 'components/ChildrenComponent';
+
+function App() {
+  return (
+    <div className=''>
+      <h1>useState Hooks</h1>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 10-useState/src/components/ChildrenComponent/index.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+  return (
+    <div className=''>
+      <p>MyComponent</p>
+    </div>
+  );
+};
+
+export default MyComponent;
