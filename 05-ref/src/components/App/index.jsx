68a729cd484c4cb1b52b8d3ca629e23549aa0a86
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-20 10:46:41 +0200

    Initialize new training projet "Ref"

---
+++ 05-ref/src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import styles from './style.module.css';
+import MyRef from 'components/MyRef';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <MyRef />
+    </div>
+  );
+}
+
+export default App;
---
+++ 05-ref/src/components/MyRef/index.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import styles from './style.module.css';
+
+class MyRef extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Valeur: </h1>
+        <input type='text' />
+      </div>
+    );
+  }
+}
+
+export default MyRef;
