38569f37b5d9a2b8b8936a440dad100073beb7ac
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-20 17:18:45 +0200

    Add redirection on tutorial page

--- 07-react-router-5/src/components/App/index.jsx
+++ 07-react-router-5/src/components/App/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import styles from './style.module.css';
 import Docs from 'components/Docs';
 import Tutorial from 'components/Tutorial';
@@ -7,21 +7,35 @@ import Community from 'components/Community';
 import NavBar from 'components/NavBar';
 import ErrorPage from 'components/ErrorPage';
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route exact path='/' component={() => <h1>Accueil</h1>} />
-          <Route exact path='/Docs' component={Docs} />
-          <Route exact path='/tutorial' component={Tutorial} />
-          <Route exact path='/community' component={Community} />
-          <Route component={ErrorPage} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      underConstruction: {
+        Docs: false,
+        Tutorial: true,
+        Community: false
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles.App}>
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route exact path='/' component={() => <h1>Accueil</h1>} />
+            <Route exact path='/Docs' component={Docs} />
+            <Route exact path='/tutorial' component={Tutorial} />
+            <Route exact path='/community' component={Community} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- 07-react-router-5/src/components/Tutorial/index.jsx
+++ 07-react-router-5/src/components/Tutorial/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './style.module.css';
 
-const Tutorial = () => {
+const Tutorial = (props) => {
+  setTimeout(() => {
+    props.history.push('/');
+  }, 5000);
+
   return (
     <div className={styles.Tutorial}>
-      <h1>You are on page "Tutorial"</h1>
+      <h1 style={{ color: 'orange' }}>
+        This page is under construction. You will be redirected to the homepage
+        in 5 seconds
+      </h1>
     </div>
   );
 };
