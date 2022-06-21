915e79410abd53658ef91839c917eeb341f7dbb8
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-21 22:41:19 +0200

    Training on index nested route

--- 21-react-router-6/src/components/App/index.jsx
+++ 21-react-router-6/src/components/App/index.jsx
@@ -16,7 +16,10 @@ const App = () => {
         <Route path='/' element={<Navigate to='/home' />} />
         <Route path='/home' element={<h1>Accueil</h1>} />
         <Route path='/docs' element={<Docs />}>
+          <Route index element={<Navigate to='installation' />} />
           <Route path='installation' element={<h1>Installation</h1>} />
+          <Route path='fondamentaux' element={<h1>Fondamentaux</h1>} />
+          <Route path='hooks' element={<h1>Hooks</h1>} />
         </Route>
         <Route path='/tutorial' element={<Tutorial />} />
         <Route path='/community' element={<Community />} />
--- 21-react-router-6/src/components/Docs/index.jsx
+++ 21-react-router-6/src/components/Docs/index.jsx
@@ -2,14 +2,25 @@ import { NavLink, Outlet } from 'react-router-dom';
 import styles from './style.module.css';
 
 const Docs = () => {
+  const navLinkClass = ({ isActive }) => {
+    const className = '';
+    return isActive ? `${className} ${styles.active}` : `${className}`;
+  };
+
   return (
     <div className={styles.Docs}>
       <h1>You are on page "Docs"</h1>
       <hr />
       <nav>
-        <NavLink to='installation'>Installation</NavLink>
-        <NavLink to='fondamentaux'>Fondamentaux</NavLink>
-        <NavLink to='hooks'>Hooks</NavLink>
+        <NavLink to='installation' className={navLinkClass}>
+          Installation
+        </NavLink>
+        <NavLink to='fondamentaux' className={navLinkClass}>
+          Fondamentaux
+        </NavLink>
+        <NavLink to='hooks' className={navLinkClass}>
+          Hooks
+        </NavLink>
       </nav>
 
       <Outlet />
