a0f4b29ba8d99c2004ba9f9f9c3a9d5a49e029f2
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-24 16:53:20 +0200

    delete project 22-shiny

--- 22-shiny-agency-oc/src/pages/Home/index.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className='Home'>
-      <h1>Home 🏠</h1>
-    </div>
-  );
-}
-
-export default App;
--- 22-shiny-agency-oc/src/pages/Survey/index.jsx
+++
@@ -1,9 +0,0 @@
-function Survey() {
-  return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-    </div>
-  );
-}
-
-export default Survey;
